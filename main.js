fd200298c4100d1a46bf80a35a59109405c46738
Dante Godinho <110043723+DanteGDF@users.noreply.github.com>
2024-04-12 19:38:42 -0300

    main.js

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -37,7 +38,7 @@ function calculaTempo(tempoObjetivo) {
         return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
-    }   
+    }
 }
 
 function atualizaCronometro(){
@@ -45,12 +46,11 @@ function atualizaCronometro(){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
-
 function comecaCronometro(){
-    atualizaCronometro ();
+    atualizaCronometro();
     setInterval(atualizaCronometro,1000);
 }
 
